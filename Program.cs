f5be151e4db2226fefb8f07f16991631e9d100c5
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-19 14:47:41 -0300

    Solução do erxercício medidas

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio_Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double metros= 0;
+            double cm=0;
+            double km=0;
+
+            Console.ForegroundColor = ConsoleColor. DarkBlue;
+            Console.WriteLine("Após digitar um numero ele será convertido nas unidades de medidas cm e km");
+            Console.WriteLine("");
+            Console.ResetColor();
+            
+            Console.Write("Digite um numero na unidade de medida metro: ");
+            metros=double.Parse(Console.ReadLine());
+
+            cm = metros * 100;
+            km = metros / 1000;
+
+            Console.WriteLine($"--- Equivalência ---\n{cm} cm \n{metros} m \n{km} km");
+
+            
+
+        }    
+    }
+}
